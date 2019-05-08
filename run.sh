040cb64b9872e754da335501a27e284535f0c2b0
Ving <viing937@gmail.com>
2019-05-08 10:25:57 +0800

    move docker workaround to git-bash config

--- run.sh
+++ run.sh
@@ -1,13 +1,6 @@
 #!/bin/bash -x
 
-# workround: https://github.com/docker/toolbox/issues/673#issuecomment-355275054
-docker()
-{
-    (export MSYS_NO_PATHCONV=1; "docker.exe" "$@")
-}
-
 DIR=`pwd`
-
 NOTEBOOK=8888
 IMAGE=mynotebook
 docker container stop "$IMAGE"
