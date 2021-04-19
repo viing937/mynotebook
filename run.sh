9b9fdb75532e7cc71fc0046ddc045129a4519a6a
Ving <viing937@gmail.com>
2021-04-19 13:15:57 +0800

    add auth restart

--- run.sh
+++ run.sh
@@ -7,8 +7,9 @@ PORT=9999
 DIR=`pwd`
 
 docker container stop "$NAME" || true
-docker run -d -it --rm \
+docker run -d \
     --name "$NAME" \
     --publish "$PORT:8888" \
+    --restart=always \
     --volume "$DIR/shared:/root/shared" \
     "ving/mynotebook"
