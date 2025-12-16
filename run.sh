cf30b741ed008727ef9dce4d07707088691f573c
Ving <viing937@gmail.com>
2025-12-16 22:43:08 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
 set -e -o pipefail
 
-uv run --with jupyter jupyter lab --notebook-dir shared --port 19999
+uv run --with jupyter jupyter lab --notebook-dir shared --port 19999 --no-browser --IdentityProvider.token=''
